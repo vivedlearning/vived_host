babc2e785d45d3fe9e6bda58f074760be00f5cf5
Amos Patrick <amos.patrick@vivedlearning.com>
2021-12-07 06:37:24 -0700

    Fixed a couple lint errors

--- src/Entities/ObserverList.ts
+++ src/Entities/ObserverList.ts
@@ -1,20 +1,20 @@
 export class ObserverList<T> {
-  private observers: { (msg: T): void }[] = [];
+  private observers: ((msg: T) => void)[] = [];
 
   public notify = (msg: T) => {
     this.observers.forEach((obs) => {
       obs(msg);
     });
-  }
+  };
 
   public add = (obs: (msg: T) => void) => {
     this.observers.push(obs);
-  }
+  };
 
   public remove = (obs: (msg: T) => void) => {
     const index = this.observers.indexOf(obs);
     if (index >= 0) {
       this.observers.splice(index, 1);
     }
-  }
+  };
 }
